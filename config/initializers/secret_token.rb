0cadcdc8a4b16bb353e6c8d83c837e656b41b2f7
Sean Devine <barelyknown@icloud.com>
2012-10-21 08:52:50 -0500

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daringfiresearch::Application.config.secret_token = '00cae2f62834690b5e036df504c9411a445c8fff883174ef9e16aea112669d1b8deec9542e5d4225dab3095a2e351d2af6054924bf9e2640e330f9c81032bea8'
